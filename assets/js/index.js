eb395f8af7c0603886b074ea4a0c95df45198967
Pavel <elena71vd@gmail.com>
2021-01-29 08:53:25 +0200

    fix code

--- assets/js/index.js
+++ assets/js/index.js
@@ -13,32 +13,28 @@ form.addEventListener('submit', (e) => {
     target,
     target: { email: emailInput },
   } = e;
+  
   arr.push(emailInput.value);
   console.dir(emailInput.value);
+
   if (emailInput.value) {
-      const newLi =  createLi(emailInput.value);
-      const newButton =  createButton();
-      ulElem.append(newLi);
-      newLi.append(newButton);
-      newButton.addEventListener('click', (e) => {
-        newButton.parentNode.remove();
-        const parrentText = newButton.parentNode.textContent.slice(0, -6);
-        console.log(parrentText);
-        const i = arr.indexOf(parrentText);
-        arr.splice(i, 1);
-      });
+    const newLi =  createElement('li', emailInput.value);
+    const newButton =  createElement('button', 'delete');
+
+    ulElem.append(newLi);
+    newLi.append(newButton);
+
+    newButton.addEventListener('click', () => {
+      newButton.parentNode.remove();
+      arr.splice(arr.indexOf(newButton.parentNode.textContent.slice(0, -6)), 1);
+    });
   }
+
   target.reset();
 });
 
-function createLi(value) {
-  const liElement = document.createElement('li');
-  liElement.textContent = value;
-  return liElement;
-}
-
-function createButton() {
-  const buttonElement = document.createElement('button');
-  buttonElement.textContent = 'delete';
-  return buttonElement;
+function createElement(tagName, tagContent) {
+  const element = document.createElement(tagName);
+  element.textContent = tagContent;
+  return element;
 }
